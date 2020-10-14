b0a1138f6eed8564250201a6f42b632685712503
chenyuming <licmucm@gmail.com>
2020-10-14 18:13:56 +0800

    [feat] change log

--- main.go
+++ main.go
@@ -1,23 +1,16 @@
 package main
 
 import (
-	"flag"
-	_ "flag"
 	"github.com/gin-gonic/gin"
 	"io/ioutil"
-	"k8s.io/klog/v2"
+	"os"
 )
 
 func main() {
 	r := gin.Default()
-	klog.InitFlags(nil)
-	_ = flag.Set("logtostderr", "false")
-	_ = flag.Set("log_file", "myfile.log")
-	flag.Parse()
 
 	r.GET("/ping", func(c *gin.Context) {
-		klog.Info("Hello Rancher")
-		klog.Flush()
+		_, _ = os.Stderr.WriteString("your message here")
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
@@ -25,8 +18,7 @@ func main() {
 
 	r.POST("/pushover/webhook", func(c *gin.Context) {
 		body, _ := ioutil.ReadAll(c.Request.Body)
-		klog.Info(string(body))
-		klog.Flush()
+		_, _ = os.Stderr.WriteString(string(body))
 
 		c.JSON(200, gin.H{
 			"message": string(body),
