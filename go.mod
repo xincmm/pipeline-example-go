8210992af7d416e8c15b49726ea67b06f85e2965
chenyuming <licmucm@gmail.com>
2020-10-14 17:42:25 +0800

    [feat] add klog

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module pipeline-example-go
 
 go 1.13
 
-require github.com/gin-gonic/gin v1.6.3
+require (
+	github.com/gin-gonic/gin v1.6.3
+	k8s.io/klog/v2 v2.3.0
+)
--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"flag"
+	_ "flag"
 	"github.com/gin-gonic/gin"
 	"io/ioutil"
+	"k8s.io/klog/v2"
 )
 
 func main() {
 	r := gin.Default()
+	klog.InitFlags(nil)
+	_ = flag.Set("logtostderr", "false")
+	_ = flag.Set("log_file", "myfile.log")
+	flag.Parse()
+
 	r.GET("/ping", func(c *gin.Context) {
-		println("Hello Rancher")
+		klog.Info("Hello Rancher")
+		klog.Flush()
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
@@ -16,7 +25,9 @@ func main() {
 
 	r.POST("/pushover/webhook", func(c *gin.Context) {
 		body, _ := ioutil.ReadAll(c.Request.Body)
-		println(string(body))
+		klog.Info(string(body))
+		klog.Flush()
+
 		c.JSON(200, gin.H{
 			"message": string(body),
 		})
